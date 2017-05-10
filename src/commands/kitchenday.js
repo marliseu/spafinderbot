24db262296fa39d6f738acf5cd668f517940d2c3
Marlise Urscheler <marlise.au@gmail.com>
2017-05-10 18:27:35 -0400

    Modified formatting for kitchenday.js

--- src/commands/kitchenday.js
+++ src/commands/kitchenday.js
@@ -24,7 +24,7 @@ const handler = (payload, res) => {
     let attachments = [{
         title: "Today's kitchen day is... ",
         color: '#00bcb4',
-        text: "> *" + status + "*! \n" +
+        text: "> *" + status + "* 👠 \n" +
             "Cinderelly, Cinderelly... Night and day it's Cinderelly, Make the fire, fix the breakfast... Wash the dishes, do the mopping!",
         mrkdwn_in: ['text']
     }]
