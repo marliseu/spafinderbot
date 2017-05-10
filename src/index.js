45d5d7ce2605030e1d6844e0d9672412b43ec829
Marlise Urscheler <marlise.au@gmail.com>
2017-05-10 17:03:58 -0400

    Modified slash command

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 'use strict'
 
 const express = require('express')
@@ -14,9 +13,9 @@ let bot = require('./bot')
 let app = express()
 
 if (config('PROXY_URI')) {
-  app.use(proxy(config('PROXY_URI'), {
-    forwardPath: (req, res) => { return require('url').parse(req.url).path }
-  }))
+    app.use(proxy(config('PROXY_URI'), {
+        forwardPath: (req, res) => { return require('url').parse(req.url).path }
+    }))
 }
 
 app.use(bodyParser.json())
@@ -24,31 +23,31 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => { res.send('\n 👋 🌍 \n') })
 
-app.post('/commands/starbot', (req, res) => {
-  let payload = req.body
+app.post('/commands/kitchenday', (req, res) => {
+    let payload = req.body
 
-  if (!payload || payload.token !== config('STARBOT_COMMAND_TOKEN')) {
-    let err = '✋  Star—what? An invalid slash token was provided\n' +
-              '   Is your Slack slash token correctly configured?'
-    console.log(err)
-    res.status(401).end(err)
-    return
-  }
+    if (!payload || payload.token !== config('STARBOT_COMMAND_TOKEN')) {
+        let err = '✋  Star—what? An invalid slash token was provided\n' +
+            '   Is your Slack slash token correctly configured?'
+        console.log(err)
+        res.status(401).end(err)
+        return
+    }
 
-  let cmd = _.reduce(commands, (a, cmd) => {
-    return payload.text.match(cmd.pattern) ? cmd : a
-  }, helpCommand)
+    let cmd = _.reduce(commands, (a, cmd) => {
+        return payload.text.match(cmd.pattern) ? cmd : a
+    }, helpCommand)
 
-  cmd.handler(payload, res)
+    cmd.handler(payload, res)
 })
 
 app.listen(config('PORT'), (err) => {
-  if (err) throw err
+    if (err) throw err
 
-  console.log(`\n🚀  Starbot LIVES on PORT ${config('PORT')} 🚀`)
+    console.log(`\n🚀  Starbot LIVES on PORT ${config('PORT')} 🚀`)
 
-  if (config('SLACK_TOKEN')) {
-    console.log(`🤖  beep boop: @starbot is real-time\n`)
-    bot.listen({ token: config('SLACK_TOKEN') })
-  }
+    if (config('SLACK_TOKEN')) {
+        console.log(`🤖  beep boop: @starbot is real-time\n`)
+        bot.listen({ token: config('SLACK_TOKEN') })
+    }
 })
